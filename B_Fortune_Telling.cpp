217672dab3780d73f8ef0d3ed3f57ccef7c387b0
AnuragPatel1 <anuragjnvjhansi@gmail.com>
2025-04-15 18:22:08 +0530

    15-4-25

--- B_Fortune_Telling.cpp
+++ B_Fortune_Telling.cpp
@@ -24,6 +24,7 @@ signed main() {
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
     
+    
     int t;
     std::cin >> t;
     
---
+++ F_Trulimero_Trulicina.cpp
@@ -0,0 +1,112 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define PI (3.141592653589)
+#define M 1000000007
+#define pb push_back
+#define f first
+#define s second
+#define rep(i,j) for(int i = 0; i<j; i++)
+#define rrep(i,j) for(int i = j; i>=0; i--)
+#define all(x) x.begin(), x.end()
+#define print(x) cout << x << endl
+#define YES cout<<"YES"<<endl
+#define NO cout<<"NO"<<endl
+#define pm cout<<"-1"<<endl
+
+//Typedef
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef map<int,int> mii;
+typedef set<int,int> sii;
+
+// Debug Overloads
+#ifdef localenv
+#define debug(x) _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.f); cerr << ","; _print(p.s); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+// Operator Overloads
+template<typename T> // cin >> vector<T>
+istream& operator>>(istream &istream, vector<T> &v){for (auto &it : v)cin >> it;return istream;}
+template<typename T> // cout << vector<T>
+ostream& operator<<(ostream &ostream, const vector<T> &c) { for (auto &it : c) cout << it << " "; return ostream; }
+
+void solve(){
+    int n,m,k; cin >> n >> m >> k;
+    
+    int freq = (n*m)/k;
+    int start = 1;
+    int d = freq;
+    vector<vi>arr(n,vi(m,0));
+
+    rep(i,n){
+        rep(j,m){
+           arr[i][j] = start++;
+           if(start > k) start = 1;
+        }
+    }
+
+    if(m % k == 0 && k <= m){
+        rep(i,n){
+            if(i == 0) continue;
+            // int last = arr[i][m-1];
+            for(int j = m-1; j > 0; j--){
+               arr[i][j] = arr[i-1][j-1];
+               if(start > k) start = 1;
+            }
+            arr[i][0] = arr[i-1][m-1];
+        }
+    }
+
+
+   for(auto i:arr){
+      for(auto j:i){
+        cout << j <<" " ;
+      }
+      cout << endl;
+   }
+
+
+}
+
+int main()
+{
+ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int t; 
+    // t = 1;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
+
